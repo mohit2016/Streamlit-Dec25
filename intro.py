d9c3b497ad08f2fc2da4746ea1a32a1cfebe58ad
Mohit Uniyal <mohituniyal2010@gmail.com>
2025-12-06 07:51:50 +0530

    add interactive elements

--- intro.py
+++ intro.py
@@ -6,3 +6,19 @@ st.subheader("Deploying using :blue[Streamlit Cloud]")
 
 st.write("Learning streamlit for the first time.")
 
+agree = st.checkbox("I agree with Mohit.")
+
+if agree:
+    st.write("You're Great!")
+
+
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    ["Comedy", "Drama", "Documentary"],
+)
+
+if genre == "Comedy":
+    st.write("You comedy me... Hahaha!")
+elif genre == "Drama":
+    st.write("You drama me... Boohoo!")
